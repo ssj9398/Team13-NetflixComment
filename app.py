67d7a42a31165b266bcde781ca6d6443c97f2444
Lim Hyunwoo <hyunwoome@icloud.com>
2022-01-11 22:37:43 +0900

    feat : 메인페이지 UI 생성

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from home import get_contents
+from home import home_page, get_movies,save_movies
 from detail import detail
 
 app = Flask(__name__)
 
-app.register_blueprint(get_contents)
+app.register_blueprint(home_page)
+app.register_blueprint(get_movies)
+app.register_blueprint(save_movies)
 app.register_blueprint(detail)
 
 
