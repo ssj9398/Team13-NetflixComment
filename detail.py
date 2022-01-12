ac6987c2bc40c5729b25a75d541d87d12e1e2da3
alisyabob <sjs939883@gmail.com>
2022-01-12 22:52:27 +0900

    상세페이지 리뷰(수정, 삭제, 등록) 완료 및 디자인 수정

--- detail.py
+++ detail.py
@@ -16,6 +16,9 @@ detail = Blueprint('detail', __name__)
 
 @detail.route('/detail/<category>/<movie_name>')
 def main(movie_name, category):
+    from app import GetJwtId
+    TokenUserId = GetJwtId()
+
     movie_name = urllib.parse.quote(movie_name, safe='')
     category = parse.quote(category)
     url = f'https://www.justwatch.com/kr/{category}/{movie_name}'
@@ -39,6 +42,8 @@ def main(movie_name, category):
     movieImage = soup.select_one(
         '#base > div.jw-info-box > div > div.jw-info-box__container-sidebar > div > aside > div.hidden-xs.visible-sm.hidden-md.hidden-lg.title-sidebar__desktop > div > picture > source:nth-child(1)')[
         "data-srcset"].split(',')[0]
+
+    ###base > div.jw-info-box > div > div.jw-info-box__container-sidebar > div > aside > div.hidden-sm.visible-md.visible-lg.title-sidebar__desktop > div.title-poster.title-poster--no-radius-bottom > picture > source:nth-child(1)
     print("movieImage = " + movieImage)
 
     movieSummary = soup.select_one(
@@ -46,41 +51,105 @@ def main(movie_name, category):
 
     dramaSummary = soup.select_one(
         '#base > div.jw-info-box > div > div.jw-info-box__container-content > div:nth-child(2) > div:nth-child(7) > div:nth-child(1) > div:nth-child(4) > p > span')
-         ##base > div.jw-info-box.jw-info-box--no-card > div > div.jw-info-box__container-content > div:nth-child(2) > div:nth-child(6) > div:nth-child(1) > div:nth-child(4) > p
+    ##base > div.jw-info-box.jw-info-box--no-card > div > div.jw-info-box__container-content > div:nth-child(2) > div:nth-child(6) > div:nth-child(1) > div:nth-child(4) > p
+
+    # dramaMainSomnail = soup.select_one(
+    #     '#base > div.backdrops > div > div > div:nth-child(2) > picture > img')["src"]
+    #
+    # print("dramaMainSomnail = " +dramaMainSomnail.text)
+
+
     if str(movieSummary) != 'None':
         movieSummary = movieSummary.text
 
     elif str(movieSummary) == 'None':
         movieSummary = dramaSummary.text
 
-    return render_template('detail.html', movieTitle=movieTitle, movieGenre=movieGenre, movieTime=movieTime,
-                           movieSummary=movieSummary, movieImage=movieImage)
+    read_reviews()
+
+    return render_template('detail.html', TokenUserId=TokenUserId, movieTitle=movieTitle, movieGenre=movieGenre,
+                           movieTime=movieTime,
+                           movieSummary=movieSummary, movieImage=movieImage, reviews=reviews)
 
 
 @detail.route('/review', methods=['POST'])
 def write_review():
-    print(movieTitle)
+    from app import GetJwtId
+    global TokenUserId
+    TokenUserId = GetJwtId()
     now = datetime.datetime.now()
     review = request.form['Review']
     review = review.replace("  ", " ")
     starValue = request.form['starValue']
-    #print("Review length"+len(str(Review)))
-    print("Review = " +review + "starValue = " +starValue)
-    if starValue != "" and review !="":
+
+    if starValue != "" and review != "":
         nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
 
         doc = {
+            'userId': TokenUserId,
             'Review': review,
             'starValue': starValue,
             'movieTitle': movieTitle,
             'nowDatetime': nowDatetime
         }
         db.review.insert_one(doc)
-
-        return jsonify({'msg': '이 요청은 POST!'})
+        return jsonify({'msg': '리뷰 등록 완료'})
+        # return render_template('detail.html', userId=TokenUserId, review=review, starValue=starValue,
+        #                        nowDatetime=nowDatetime)
 
 
 @detail.route('/review', methods=['GET'])
 def read_reviews():
-    reviews = list(db.review.find({"movieTitle":movieTitle}, {'_id': False}))
-    return jsonify({'all_reviews': reviews})
+    from app import GetJwtId
+    TokenUserId = GetJwtId()
+    global reviews
+    reviews = list(db.review.find({"movieTitle": movieTitle}, {'_id': False}))
+    print(reviews)
+    # return jsonify({'reviews': reviews, 'TokenUserId': TokenUserId})
+    return render_template('detail.html', reviews=reviews, TokenUserId=TokenUserId)
+
+
+@detail.route('/review', methods=['Delete'])
+def del_reviews():
+    from app import GetJwtId
+    TokenUserId = GetJwtId()
+    userId = request.form['userid']
+    Review = request.form['review']
+    starValue = request.form['starValue']
+    nowDatetime = request.form['writeTime']
+    if (TokenUserId == userId):
+        db.review.delete_one({'userId': userId, 'nowDatetime': nowDatetime, 'Review': Review, 'starValue': starValue})
+        review = db.review.find({'movieTitle': movieTitle})
+        print(review)
+        return jsonify({'msg': '삭제완료'})
+    else:
+        return jsonify({'msg': '본인이 작성한 리뷰만 삭제가능합니다.'})
+
+
+@detail.route('/review', methods=['Put'])
+def update_reviews():
+    from app import GetJwtId
+    TokenUserId = GetJwtId()
+    userId = request.form['userid']
+    Review = request.form['review']
+    starValue = request.form['starValue']
+    nowDatetime = request.form['writeTime']
+    updateReview = request.form['updateReview']
+    updateStarValue = request.form['updateStarValue']
+    print("userId = " + userId + "Review = " + Review + "starValue = " + starValue + "nowDatetime = " + nowDatetime)
+    if (TokenUserId == userId):
+        db.review.update_one({'userId': userId, 'nowDatetime': nowDatetime, 'Review': Review, 'starValue': starValue},
+                             {'$set': {'Review': updateReview, 'starValue': updateStarValue}})
+        review = db.review.find({'movieTitle': movieTitle})
+        print(review)
+        return jsonify({'msg': '수정완료'})
+    else:
+        return jsonify({'msg': '본인이 작성한 리뷰만 수정가능합니다.'})
+
+
+@detail.route('/allReview', methods=['GET'])
+def read_all_Reviews():
+    global reviews
+    reviews = list(db.review.find({}, {'_id': False}))
+    print(reviews)
+    return jsonify({'reviews': reviews})
